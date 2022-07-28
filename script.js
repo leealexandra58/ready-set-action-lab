e5110e7a8dcef4de0350f9c9472bcbd50f958577
leeal <leealexandra58@gmail.com>
2022-07-28 19:01:47 -0400

    finished

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // 1
+const main = () => {
 
 const addClass = document.querySelector('#grow-me')
 addClass.classList.add('big')
@@ -14,9 +15,14 @@ console.log(listNames[0].innerText);
 console.log(listNames[1].innerText);
 console.log(listNames[2].innerText);
 
-// 4
+/* another way to do this 
+document.querySelectorAll('li').forEach((node) => {
+    console.log(node.innerText);
+)};
+*/
 
-const link = document.getElementById("link")
+// 4
+const link = document.querySelector('.link')
 link.href = "http://www.example.com/";
 link.textContent = 'somewhere'
 
@@ -26,8 +32,15 @@ const displayNone = document.querySelector('#hide-me')
 displayNone.style.display = "none"
 
 // 6
+
 const displayBlock = document.querySelector('#show-me')
 displayBlock.style.display = "block"
 
 // 7
 
+const name = document.getElementById('name').value;
+console.log(name);
+
+document.querySelector('h1').innerText = `Welcome ${name}`;
+
+};
